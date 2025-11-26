383ed60fb5eb63bfd0a32e0cab1bae4198e4de1e
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2025-11-26 07:21:45 -0300

    Iniciandoo projeto

---
+++ test/login.test.js
@@ -0,0 +1,20 @@
+const mocha = require('mocha');
+const {expect} = require('chai');
+const request = require('supertest');
+
+describe('Login', () => {
+    describe('POST /login',() => {
+        it('Deve retornar 200 com token em string quando usa credenciais válidas', async () => {
+            const resposta = await request('http://localhost:3000')
+              .post('/login') // Substitua pela URL correta do seu servidor
+              .set('Content-Type', 'application/json') // Define o cabeçalho Content-Type
+              .send({
+                        'username': 'gustavo',
+                        'senha': '123456'
+              })
+              expect(resposta.status).to.equal(200);
+              expect(resposta.body.token).to.be.a('string');
+              
+        })
+    })
+});
