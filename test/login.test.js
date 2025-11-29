19fb2d3f14959558af639bd7e6490b0afbabbe27
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2025-11-29 09:56:29 -0300

    Adicionanco fixtures para gerir os corpos das requisições

--- test/login.test.js
+++ test/login.test.js
@@ -2,18 +2,16 @@ const mocha = require('mocha');
 const {expect} = require('chai');
 const request = require('supertest');
 require('dotenv').config();
-
+const postLogin = require('../fixtures/postLogin.json');
 
 describe('Login', () => {
     describe('POST /login',() => {
         it('Deve retornar 200 com token em string quando usa credenciais válidas', async () => {
+            const bodyLogin = {...postLogin};
             const resposta = await request(process.env.BASE_URL)
               .post('/login') // Substitua pela URL correta do seu servidor
               .set('Content-Type', 'application/json') // Define o cabeçalho Content-Type
-              .send({
-                        'username': 'gustavo',
-                        'senha': '123456'
-              })
+              .send(bodyLogin); // Envia o corpo da requisição com as credenciais de login  
               expect(resposta.status).to.equal(200);
               expect(resposta.body.token).to.be.a('string');
               
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -3,41 +3,33 @@ const { expect } = require("chai");
 const request = require("supertest");
 require("dotenv").config();
 const autenticacao = require("../helpers/autenticacao");
+const postTransferencia = require("../fixtures/postTransferencia.json");
 
 describe("Transferências", () => {
   describe("POST /transferencias", () => {
     let token;
     beforeEach(async () => {
-       token = await autenticacao.obterToken();
+      token = await autenticacao.obterToken();
     });
     it("Deve retornar 201 quando a transferência for acima de R$10,00", async () => {
       // Capturando o token de autenticação
+      const bodyTransferencia = { ...postTransferencia }; // Usando o fixture para o corpo da transferência
       const resposta = await request(process.env.BASE_URL)
         .post("/transferencias")
         .set("Content-Type", "application/json")
         .set("Authorization", `Bearer ${token}`) // Adiciona o token no cabeçalho Authorization
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 10.0,
-          token: "",
-        });
+        .send(bodyTransferencia); // Usando o corpo da transferência do fixture
       expect(resposta.status).to.equal(201);
       console.log(resposta.body);
     });
 
     it("Deve retornar 422 quando a transferência for abaixo de R$10,00", async () => {
-      // Capturando o token de autenticação
+      const bodyTransferencia = { ...postTransferencia, valor: 9.99 }; // Modificando o valor para abaixo de R$10,00
       const resposta = await request(process.env.BASE_URL)
         .post("/transferencias")
         .set("Content-Type", "application/json")
         .set("Authorization", `Bearer ${token}`) // Adiciona o token no cabeçalho Authorization
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 9.99,
-          token: "",
-        });
+        .send(bodyTransferencia); // Usando o corpo da transferência do fixture
       expect(resposta.status).to.equal(422);
       console.log(resposta.body);
     });
